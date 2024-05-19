715a239ca9f467f01aa487b3e0bccce675786b9f
adiomi90 <adiomi90@outlook.com>
2024-05-19 12:17:07 +0300

    The landing page

--- database_connection.py
+++ database_connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "mysql+mysqlconnector://root:yourpassword@localhost/bookbuddy"
+DATABASE_URL = "mysql+mysqlconnector://root:inkboy90@localhost/bookbuddy"
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
